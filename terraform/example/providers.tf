47f79c11ace5c4942d387d5a465539ea864c4786
Tomeista <tomeista@web.de>
2026-07-06 14:22:41 +0200

    feat: Update configurations for IPv6 support across services and add deployment guide

---
+++ terraform/example/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+    }
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+    }
+  }
+}
+
+provider "openstack" {
+  domain_name = "default"
+}
